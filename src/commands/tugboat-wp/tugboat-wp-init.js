9aff8209c76bdbdc9c41ad9a635f42af5327d534
Aaron Mazade <aaron@stevensinc.com>
2023-08-04 16:18:17 -0400

    move config location

--- src/commands/tugboat-wp/tugboat-wp-init.js
+++ src/commands/tugboat-wp/tugboat-wp-init.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
-const DEFAULT_CONFIG_PATH = path.join(__dirname, '../default.config.js');
+const DEFAULT_CONFIG_PATH = path.join(__dirname, '../../util/default.config.js');
 const CURRENT_DIR = process.cwd();
 
 program
