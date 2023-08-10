1f97547a4bf4e044edae4bddac2a2b217ece36f0
Aaron Mazade <aaron@stevensinc.com>
2023-08-09 23:28:03 -0400

    Add actions, reduce repetitious code

--- src/interfaces/rsyncInterface.js
+++ src/interfaces/rsyncInterface.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const chalk = require('chalk');
-
 const execOptions = {
   stdio: 'pipe',
   maxBuffer: 1024 * 1024 * 10, // 10MB buffer size
@@ -22,11 +21,27 @@ function rsyncPush(source, destination) {
   const exclude = config.remote.exclude.join(',');
   const actualDestination = `${config.remote.ssh.user}@${config.remote.ssh.host}:${destination}`;
   const rsyncOptions = config.remote.ssh.rsync_options;
+  let command = `rsync -avz ${rsyncOptions} ${source} ${actualDestination}`;
+  let errorMessage = 'Error running rsync push command:';
+  let preActions = null;
+  let userPreActions = null;
+  let postActions = null;
+  let userPostActions = null;
+
+  // Append --exclude flag if there are exclusions
+  if (exclude.length > 0) {
+    command += ` --exclude={${exclude}}`;
+  }
 
   if (config.remote.ssh.password) {
     const password = config.remote.ssh.password;
     const tmpFolderPath = fs.mkdtempSync(path.join(os.tmpdir(), 'rsync-'));
     const passwordFilePath = path.join(tmpFolderPath, 'password.txt');
+    errorMessage = 'Error running rsync push command with password:';
+    preActions = () => {
+      fs.unlinkSync(passwordFilePath);
+    };
+
     fs.writeFileSync(passwordFilePath, password);
     exec(`chmod 600 ${passwordFilePath}`, (error, stdout, stderr) => {
       if (error) {
@@ -37,44 +52,47 @@ function rsyncPush(source, destination) {
         return;
       }
 
-      let commandWithPassword = `rsync -azv ${rsyncOptions} --password-file=${passwordFilePath} ${source} ${actualDestination}`;
+      command += ` --password-file=${passwordFilePath}`;
 
-      // Append --exclude flag if there are exclusions
-      if (exclude.length > 0) {
-        commandWithPassword += ` --exclude={${exclude}}`;
+      try {
+        executeRsync();
+      } catch (error) {
+        console.error(chalk.red(errorMessage), error);
       }
-
-      exec(commandWithPassword, execOptions, (error, stdout, stderr) => {
-        fs.unlinkSync(passwordFilePath);
-
-        if (error) {
-          console.error(
-            chalk.red('Error running rsync pull command with password:'),
-            error,
-          );
-          return;
-        }
-      });
     });
   } else {
-    let command = `rsync -avz ${rsyncOptions} ${source} ${actualDestination}`;
-
-    // Append --exclude flag if there are exclusions
-    if (exclude.length > 0) {
-      command += ` --exclude={${exclude}}`;
+    try {
+      executeRsync();
+    } catch (error) {
+      console.error(chalk.red(errorMessage), error);
     }
+  }
 
+  function executeRsync() {
     exec(command, execOptions, (error, stdout, stderr) => {
+      preActions();
+      // Call the user-defined preActions hook, if provided
+      if (userPreActions && typeof userPreActions === 'function') {
+        userPreActions();
+      }
+
       if (error) {
-        console.error(chalk.red('Error running rsync pull command:'), error);
-        return;
+        throw error;
+      }
+
+      postActions();
+      // Call the user-defined postActions hook, if provided
+      if (userPostActions && typeof userPostActions === 'function') {
+        userPostActions();
       }
     });
   }
 
-  console.log(chalk.bold('Transporting your assets to your remote server:'));
-  console.log(chalk.yellow('Source:'), source);
-  console.log(chalk.cyan('Destination:'), destination);
+  process.nextTick(() => {
+    console.log(chalk.bold('Transporting your assets to your remote server:'));
+    console.log(chalk.yellow('Source:'), source);
+    console.log(chalk.cyan('Destination:'), destination);
+  });
 }
 
 /**
@@ -117,7 +135,6 @@ function rsyncPull(source, destination) {
         }
       });
     });
-    console.log(commandWithPassword);
   } else {
     const command = `rsync -azv ${rsyncOptions} ${actualSource} ${destination}`;
 
@@ -131,13 +148,12 @@ function rsyncPull(source, destination) {
         }
       }
     });
-
-    console.log(command);
   }
-
-  console.log(chalk.bold('Transporting your assets to your local server:'));
-  console.log(chalk.yellow('Source:'), source);
-  console.log(chalk.cyan('Destination:'), destination);
+  process.nextTick(() => {
+    console.log(chalk.bold('Transporting your assets to your local server:'));
+    console.log(chalk.yellow('Source:'), source);
+    console.log(chalk.cyan('Destination:'), destination);
+  });
 }
 
 module.exports = {
