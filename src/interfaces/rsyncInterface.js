9ef258da415cb6e510a38b3265ccb6792c46ad36
Aaron Mazade <aaron@stevensinc.com>
2023-08-09 23:51:27 -0400

    refactor push and pull commands

--- src/interfaces/rsyncInterface.js
+++ src/interfaces/rsyncInterface.js
@@ -1,14 +1,25 @@
 const { exec } = require('child_process');
 const { readConfig } = require('../util/readConfig.js');
-const settings = require('../util/settings.js');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const chalk = require('chalk');
+
+const config = readConfig();
+
 const execOptions = {
   stdio: 'pipe',
   maxBuffer: 1024 * 1024 * 10, // 10MB buffer size
 };
+
+const password = config.remote.ssh.password || null;
+const tmpFolderPath = fs.mkdtempSync(path.join(os.tmpdir(), 'rsync-'));
+const passwordFilePath = path.join(tmpFolderPath, 'password.txt');
+
+let preActions = () => {};
+let userPreActions = null;
+let postActions = () => {};
+let userPostActions = null;
 
 /**
  * Pushes files using rsync from a source directory to a destination.
@@ -17,16 +28,11 @@ const execOptions = {
  * @param {string} destination - The destination directory or 'remote' to push to a remote server.
  */
 function rsyncPush(source, destination) {
-  const config = readConfig();
   const exclude = config.remote.exclude.join(',');
   const actualDestination = `${config.remote.ssh.user}@${config.remote.ssh.host}:${destination}`;
   const rsyncOptions = config.remote.ssh.rsync_options;
   let command = `rsync -avz ${rsyncOptions} ${source} ${actualDestination}`;
   let errorMessage = 'Error running rsync push command:';
-  let preActions = null;
-  let userPreActions = null;
-  let postActions = null;
-  let userPostActions = null;
 
   // Append --exclude flag if there are exclusions
   if (exclude.length > 0) {
@@ -34,9 +40,6 @@ function rsyncPush(source, destination) {
   }
 
   if (config.remote.ssh.password) {
-    const password = config.remote.ssh.password;
-    const tmpFolderPath = fs.mkdtempSync(path.join(os.tmpdir(), 'rsync-'));
-    const passwordFilePath = path.join(tmpFolderPath, 'password.txt');
     errorMessage = 'Error running rsync push command with password:';
     preActions = () => {
       fs.unlinkSync(passwordFilePath);
@@ -68,6 +71,7 @@ function rsyncPush(source, destination) {
     }
   }
 
+  // TODO: Same function for push and pull. Needs to be refactors to be moved out of current scope.
   function executeRsync() {
     exec(command, execOptions, (error, stdout, stderr) => {
       preActions();
@@ -102,15 +106,18 @@ function rsyncPush(source, destination) {
  * @param {string} destination - the destination path where the files will be transferred to.
  */
 function rsyncPull(source, destination) {
-  const config = readConfig();
-
   const actualSource = `${config.remote.ssh.user}@${config.remote.ssh.host}:${source}`;
   const rsyncOptions = config.remote.ssh.rsync_options;
 
+  let command = `rsync -azv ${rsyncOptions} ${actualSource} ${destination}`;
+  let errorMessage = 'Error running rsync push command:';
+
   if (config.remote.ssh.password) {
-    const password = config.remote.ssh.password;
-    const tmpFolderPath = fs.mkdtempSync(path.join(os.tmpdir(), 'rsync-'));
-    const passwordFilePath = path.join(tmpFolderPath, 'password.txt');
+    errorMessage = 'Error running rsync pull command with password:';
+    preActions = () => {
+      fs.unlinkSync(passwordFilePath);
+    };
+
     fs.writeFileSync(passwordFilePath, password);
     exec(`chmod 600 ${passwordFilePath}`, (error, stdout, stderr) => {
       if (error) {
@@ -121,34 +128,43 @@ function rsyncPull(source, destination) {
         return;
       }
 
-      const commandWithPassword = `rsync -azv ${rsyncOptions} --password-file=${passwordFilePath} ${actualSource} ${destination}`;
+      command += ` --password-file=${passwordFilePath}`;
 
-      exec(commandWithPassword, execOptions, (error, stdout, stderr) => {
-        fs.unlinkSync(passwordFilePath);
-
-        if (error) {
-          console.error(
-            chalk.red('Error running rsync pull command with password:'),
-            error,
-          );
-          return;
-        }
-      });
+      try {
+        executeRsync();
+      } catch (error) {
+        console.error(chalk.red(errorMessage), error);
+      }
     });
   } else {
-    const command = `rsync -azv ${rsyncOptions} ${actualSource} ${destination}`;
+    try {
+      executeRsync();
+    } catch (error) {
+      console.error(chalk.red(errorMessage), error);
+    }
+  }
 
+  // TODO: Same function for push and pull. Needs to be refactors to be moved out of current scope.
+  function executeRsync() {
     exec(command, execOptions, (error, stdout, stderr) => {
+      preActions();
+      // Call the user-defined preActions hook, if provided
+      if (userPreActions && typeof userPreActions === 'function') {
+        userPreActions();
+      }
+
       if (error) {
-        console.error(chalk.red('Error running rsync pull command:'), error);
-        return;
-      } else {
-        if (stdout.length >= execOptions.maxBuffer) {
-          console.log('Stdout maxBuffer length exceeded:', stdout.length);
-        }
+        throw error;
+      }
+
+      postActions();
+      // Call the user-defined postActions hook, if provided
+      if (userPostActions && typeof userPostActions === 'function') {
+        userPostActions();
       }
     });
   }
+
   process.nextTick(() => {
     console.log(chalk.bold('Transporting your assets to your local server:'));
     console.log(chalk.yellow('Source:'), source);
