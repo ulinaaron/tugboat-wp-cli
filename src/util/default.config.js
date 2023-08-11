1acbe140aee549d61675ebfa46c54e97f46fccd9
Aaron Mazade <aaron@stevensinc.com>
2023-08-11 14:40:05 -0400

    add in scheme for db connections

--- src/util/default.config.js
+++ src/util/default.config.js
@@ -3,6 +3,23 @@ module.exports = {
     // Local host configuration
     host: 'https://example.test', // The local host URL
     path: '/Users/name/website/public_html/', // The local path to the website files
+    database: {
+      // How would you like to connect to the database locally?
+      // Possible values: wp-cli (default), localwp, mysql
+      adapter: 'wp-cli',
+
+      // localwp: {
+      //   socket: 'path/to/localwp/mysqlld.sock'
+      // },
+
+      // mysql: {
+      //   host: 'localhost', // The MySQL host
+      //   port: 3306, // The MySQL port
+      //   user: 'root',
+      //   password: '', // Leave empty for key-based authentication
+      //   database: 'local' // The database name
+      // },
+    },
   },
   remote: {
     // Remote host configuration
@@ -26,6 +43,19 @@ module.exports = {
       password: '', // The SSH password (leave empty for key-based authentication)
       port: 22, // The SSH port
       rsync_options: '--verbose --itemize-changes', // Additional rsync options for synchronization
+    },
+    database: {
+      // How would you like to connect to the database remotely?
+      // Possible values: wp-cli (default), mysql
+      adapter: 'wp-cli',
+
+      // mysql: {
+      //   host: 'localhost', // The MySQL host
+      //   port: 3306, // The MySQL port
+      //   user: 'root',
+      //   password: '', // Leave empty for key-based authentication
+      //   database: 'local' // The database name
+      // },
     },
   },
 };
