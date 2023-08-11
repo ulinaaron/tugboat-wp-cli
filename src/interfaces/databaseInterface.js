ce3553edec0a8d76d0209794b947cd80d6d650d5
Aaron Mazade <aaron@stevensinc.com>
2023-08-11 11:03:58 -0400

    restructure db connection

--- src/interfaces/databaseInterface.js
+++ src/interfaces/databaseInterface.js
@@ -1,27 +1,27 @@
+const { spawn } = require('child_process');
 const { Client } = require('ssh2');
 const { rsyncPull, rsyncPush } = require('./rsyncInterface.js');
 const { readConfig } = require('../util/configuration.js');
 const settings = require('../util/settings.js');
 
+const config = readConfig();
+
 async function databaseProcess(direction) {
-  const config = readConfig();
+  const { host, user, password, port } = config.remote.ssh;
+
+  const connectOptions = {
+    host,
+    port,
+    username: user,
+  };
+
+  if (password !== null) {
+    connectOptions.password = password;
+  }
 
   if (direction === 'pull') {
-    // const { host, user, password, port } = config.remote.ssh;
-    const { host, user, password, port } = config.remote.ssh;
-
-    const connectOptions = {
-      host,
-      port,
-      username: user,
-    };
-
-    if (password !== null) {
-      connectOptions.password = password;
-    }
-
     const conn = new Client();
-    conn
+    await conn
       .on('ready', () => {
         console.log('SSH connection established');
 
@@ -37,73 +37,22 @@ async function databaseProcess(direction) {
             console.log('Database export started');
 
             stream
+              .on('data', (data) => {
+                console.log(data.toString()); // Real-time output from stdout
+              })
+              .stderr.on('data', (data) => {
+                console.error(data.toString()); // Real-time output from stderr
+              })
               .on('close', (code, signal) => {
                 console.log('Database export completed');
                 conn.end();
-                // rsyncPull(
-                //   config.remote.path + settings.components.database,
-                //   config.local.path,
-                // );
 
-                // const importCommand =
-                //   'wp db import ' +
-                //   config.local.path +
-                //   settings.components.database;
-                // const deleteCommand =
-                //   'rm ' + config.remote.path + settings.components.database;
+                rsyncPull(
+                  config.remote.path + settings.components.database,
+                  config.local.path,
+                );
 
-                // conn.exec(importCommand, (err, stream) => {
-                //   if (err) {
-                //     console.error('Error importing the database:', err);
-                //     conn.end();
-                //     return;
-                //   }
-
-                //   console.log('Database import started');
-
-                //   stream
-                //     .on('close', (code, signal) => {
-                //       console.log('Database import completed');
-                //       conn.end();
-
-                //       conn.exec(deleteCommand, (err, stream) => {
-                //         if (err) {
-                //           console.error(
-                //             'Error deleting the database file:',
-                //             err,
-                //           );
-                //           conn.end();
-                //           return;
-                //         }
-
-                //         console.log('Database file deleted');
-
-                //         stream
-                //           .on('close', (code, signal) => {
-                //             console.log('Database file deleted successfully');
-                //             conn.end();
-                //           })
-                //           .stderr.on('data', (data) => {
-                //             console.error(
-                //               'Error deleting the database file:',
-                //               data.toString(),
-                //             );
-                //             conn.end();
-                //           });
-                //       });
-                //     })
-                //     .stderr.on('data', (data) => {
-                //       console.error(
-                //         'Error importing the database:',
-                //         data.toString(),
-                //       );
-                //       conn.end();
-                //     });
-                // });
-              })
-              .stderr.on('data', (data) => {
-                console.error('Error exporting the database:', data.toString());
-                conn.end();
+                importDatabase();
               });
           },
         );
@@ -116,6 +65,25 @@ async function databaseProcess(direction) {
       })
       .connect(connectOptions);
   }
+}
+
+function importDatabase() {
+  const importCommand =
+    'wp db import ' + config.local.path + settings.components.database;
+
+  const importProcess = spawn(importCommand, { shell: true });
+
+  importProcess.stdout.on('data', (data) => {
+    console.log(data.toString()); // Real-time output from stdout
+  });
+
+  importProcess.stderr.on('data', (data) => {
+    console.error(data.toString()); // Real-time output from stderr
+  });
+
+  importProcess.on('close', (code) => {
+    console.log('Database import completed');
+  });
 }
 
 module.exports = databaseProcess;
